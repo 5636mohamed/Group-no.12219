e258debf8499a351388d660c2e8e135680b2e3e1
5636mohamed <85107448+5636mohamed@users.noreply.github.com>
2022-05-06 19:58:27 +0200

    Add files via upload
    These are the files that have been used in the capstone project.

---
+++ Feedback control mechanism code Group no.12219.ino
@@ -0,0 +1,28 @@
+int sensor = A5;
+int buzzer = 3;
+float resistor = 326.6;
+void setup() {
+ pinMode(sensor,INPUT);
+ pinMode(buzzer,OUTPUT);
+ Serial.begin(9600);
+ 
+}
+
+void loop() {
+  int sensorvalu = analogRead(sensor);
+  float voltsensor = sensorvalu * (5.0/1023);
+  float voltresistor = 5 - voltsensor;
+  float sensorresist = (voltsensor * resistor)/voltresistor;
+  int temperature = (sensorresist - 105)/.33333;
+  Serial.println(temperature);
+  delay(500);
+
+  if (!(temperature < 60 && temperature > 50)) {
+    for(int i=0;i<3;i++){
+  digitalWrite (buzzer, 1);
+  delay (500);
+  digitalWrite (buzzer, 0);
+  delay (500);
+
+  }}
+}
