e0544cd1a85495f8ac934328478d4770b4cd9d7b
5636mohamed <85107448+5636mohamed@users.noreply.github.com>
2022-05-06 20:27:04 +0200

    the comments that used to demonestrate the code and its usage and procedures

--- Feedback control mechanism code Group no.12219.ino
+++ Feedback control mechanism code Group no.12219.ino
@@ -1,6 +1,6 @@
-int sensor = A5;
-int buzzer = 3;
-float resistor = 326.6;
+int sensor = A5; // identifying the port of the sensor to get reads from it.
+int buzzer = 3; // identifying the port of the buzzer to take signals and apply the code
+float resistor = 326.6; // set the resistance value that is put in the circuit.
 void setup() {
  pinMode(sensor,INPUT);
  pinMode(buzzer,OUTPUT);
@@ -9,12 +9,12 @@ void setup() {
 }
 
 void loop() {
-  int sensorvalu = analogRead(sensor);
-  float voltsensor = sensorvalu * (5.0/1023);
-  float voltresistor = 5 - voltsensor;
-  float sensorresist = (voltsensor * resistor)/voltresistor;
-  int temperature = (sensorresist - 105)/.33333;
-  Serial.println(temperature);
+  int sensorvalu = analogRead(sensor); // idnetifying the postive pin of the sensor.
+  float voltsensor = sensorvalu * (5.0/1023); // identrifying the voltage that sensor had
+  float voltresistor = 5 - voltsensor; // get the voltage of the resistor by substracting the volt of the sensor from the volt of the ardiuno
+  float sensorresist = (voltsensor * resistor)/voltresistor; // get the sensor resistance to calculate the temperature due to it.
+  int temperature = (sensorresist - 105)/.33333; // the relation of between the temperature and the sensor resistance as demonstrated in its datasheet.
+  Serial.println(temperature); // print the temperature after every .5 second in the serial monitor command.
   delay(500);
 
   if (!(temperature < 60 && temperature > 50)) {
